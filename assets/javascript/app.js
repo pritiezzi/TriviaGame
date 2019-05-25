890e65926ed7625f36e0db4122452544653388c0
priscila <pristiezzi@gmail.com>
2019-05-25 12:25:59 -0500

    updated css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -81,12 +81,10 @@ $(document).ready(function () {
         running = true;
         }
     }
-    //timer countdown
+  
     function decrement() {
         $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
         timer --;
-    
-        //stop timer if reach 0
         if (timer === 0) {
             unanswerCount++;
             stop();
@@ -100,35 +98,23 @@ $(document).ready(function () {
         running = false;
         clearInterval(intervalId);
     }
-    //randomly pick question in array if not already shown
-    //display question and loop though and display possible answers
+    
     function displayQuestion() {
-        //generate random index in array
         index = Math.floor(Math.random()*options.length);
         pick = options[index];
     
-    //	if (pick.shown) {
-    //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
-    //		displayQuestion();
-    //	} else {
-    //		console.log(pick.question);
-            //iterate through answer array and display
             $("#questionblock").html("<h2>" + pick.question + "</h2>");
             for(var i = 0; i < pick.choice.length; i++) {
                 var userChoice = $("<div>");
                 userChoice.addClass("answerchoice");
                 userChoice.html(pick.choice[i]);
-                //assign array position to it so can check answer
                 userChoice.attr("data-guessvalue", i);
                 $("#answerblock").append(userChoice);
-    //		}
+   
     }
     
-    
-    
-    //click function to select answer and outcomes
+
     $(".answerchoice").on("click", function () {
-        //grab array position from userGuess
         userGuess = parseInt($(this).attr("data-guessvalue"));
     
         //correct guess or wrong guess outcomes
